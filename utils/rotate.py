c01fc7210105528fc84665248a8006d652e6f1d0
YangShuwen <374308602@qq.com>
2020-07-15 11:28:19 +0800

    update

--- utils/rotate.py
+++ utils/rotate.py
@@ -6,7 +6,7 @@ EPOCH = 1000
 
 
 def get_constrain(weight: torch.Tensor):
-    det_loss = weight.det() ** 2
+    det_loss = (weight.det() - 1) ** 2
     lc_loss = ((weight.norm(dim=0) - 1) ** 2).sum() + ((weight.norm(dim=1) - 1) ** 2).sum()
     return det_loss + lc_loss
 
@@ -15,6 +15,7 @@ def rotate_to(pos: torch.Tensor, fit_pos: torch.Tensor) -> torch.Tensor:
     linear = nn.Linear(3, 3, bias=True)
     optimizer = optim.Adam(linear.parameters(), lr=1e-2, weight_decay=1e-2)
 
+    loss_1 = 1e8
     for i in range(EPOCH):
         optimizer.zero_grad()
         new_pos = linear(pos)
@@ -23,10 +24,33 @@ def rotate_to(pos: torch.Tensor, fit_pos: torch.Tensor) -> torch.Tensor:
         if i == EPOCH - 1:
             print('fit loss:', fit_loss.item())
             print('constraint loss:', c_loss.item())
-        loss = fit_loss + c_loss * 10
+        loss = fit_loss + c_loss * 1
         loss.backward()
         optimizer.step()
+        loss_1 = loss.item()
+
+    new_pos_1 = linear(pos).detach()
+
+    pos = pos * torch.tensor([-1, 1, 1], dtype=torch.float32)
+    linear = nn.Linear(3, 3, bias=True)
+    optimizer = optim.Adam(linear.parameters(), lr=1e-2, weight_decay=1e-2)
+
+    loss_2 = 1e8
+    for i in range(EPOCH):
+        optimizer.zero_grad()
+        new_pos = linear(pos)
+        fit_loss = (new_pos - fit_pos).norm()
+        c_loss = get_constrain(linear.weight)
+        if i == EPOCH - 1:
+            print('fit loss:', fit_loss.item())
+            print('constraint loss:', c_loss.item())
+        loss = fit_loss + c_loss * 1
+        loss.backward()
+        optimizer.step()
+        loss_2 = loss.item()
+
+    new_pos_2 = linear(pos).detach()
 
     # for name, param in linear.named_parameters():
     #     print(name, ":", param)
-    return linear(pos).detach()
+    return new_pos_1 if loss_1 < loss_2 else new_pos_2
