c697ebb17bae1bf6d1149c2758967de7a8df62b0
YangShuwen <374308602@qq.com>
2020-08-23 19:05:04 +0800

    update LR and DECAY of Lipop/TOX21

--- train/config.py
+++ train/config.py
@@ -63,8 +63,8 @@ MODEL_CONFIG_QM9.update({
 
 MODEL_CONFIG_LIPOP = MODEL_CONFIG.copy()
 MODEL_CONFIG_LIPOP.update({
-    'LR': 2e-4,
-    'DECAY': 1e-4,
+    'LR': 2e-5,
+    'DECAY': 1e-3,
     'DROPOUT': 0.5,
     'ITERATION': 200,
     'BATCH': 64,
@@ -77,8 +77,8 @@ MODEL_CONFIG_LIPOP.update({
 
 MODEL_CONFIG_TOX21 = MODEL_CONFIG.copy()
 MODEL_CONFIG_TOX21.update({
-    'LR': 2e-4,
-    'DECAY': 4e-5,
+    'LR': 2e-5,
+    'DECAY': 1e-3,
     'DROPOUT': 0.5,
     'ITERATION': 200,
     'BATCH': 64,
