47f76f0c38c9a8d0cbf7a409c944cdd6a344e198
YangShuwen <374308602@qq.com>
2020-09-05 23:03:29 +0800

    fit seed

--- train/lipop_trainer.py
+++ train/lipop_trainer.py
@@ -22,7 +22,7 @@ LOG_PATH = 'logs/Lipop/'
 
 
 def set_seed(seed: int, use_cuda: bool):
-    np.random.seed(20201002)
+    np.random.seed(16880611)
     torch.manual_seed(seed)
     if use_cuda:
         torch.cuda.manual_seed(seed)
